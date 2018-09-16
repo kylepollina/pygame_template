5fa0403912507589167d129800d5a5730d8a8c31
Kyle Pollina <kylepollina@gmail.com>
2018-09-16 14:56:40 -0400

    init

---
+++ definitions.py
@@ -0,0 +1,46 @@
+# definitions.py
+#  Definitions of constants for tetris game
+#  Author: Kyle Pollina
+
+# Built in constants
+TITLE       = "Tetris"
+GAME_OVER   = 0
+RUN         = 1
+PAUSE       = 2
+WIDTH       = 20*20
+HEIGHT      = 20*30
+FPS         = 30
+
+# Define colors
+WHITE  = (255,255,255)
+BLACK  = (0,0,0)
+RED    = (255,0,0)
+GREEN  = (0,255,0)
+BLUE   = (0,0,255)
+YELLOW = (255,255,0)
+CYAN   = (0,255,255)
+PURPLE = (255,0,255)
+ORANGE = (255,140,0)
+
+# User made constants
+BOARDWIDTH  = 20*10
+BOARDHEIGHT = 20*20
+BOARDLEFT   = 5*20
+BOARDRIGHT  = 15*20
+BOARDTOP    = 5*20
+BOARDBOT    = 25*20
+
+
+TESTSPEED   = 10
+NORMSPEED   = 20
+SLOWSPEED   = 30
+
+
+# Define block types
+SQUARE = 0
+LINE   = 1
+TBLOCK = 2
+ZBLOCK = 3
+SBLOCK = 4
+JBLOCK = 5
+LBLOCK = 6
---
+++ tetris.py
@@ -0,0 +1,120 @@
+# tetris.py
+#  Classic tetris game written with Pythons Pygame Library
+#  Author: Kyle Pollina
+
+
+import pygame
+import random
+from definitions import *
+
+##############################
+# run() - code to execute the game
+#  run() is called way down at the bottom of the file
+##############################
+def run():
+    pygame_setup()
+    game_setup()
+    game_loop()
+    pygame.quit()
+
+
+##############################
+# game_loop() - code for running the game loop
+# 
+##############################
+def game_loop():
+    global running
+    running = True
+
+    while running:
+        # DO NOT EDIT
+        clock.tick(FPS)     # keeps the game running at the right speed
+        process_input()
+        update()
+        draw()
+
+
+##############################
+# update() - this is the meat and potatoes of the games code
+#  if you want to update game pieces, do it here
+##############################
+def update():
+    i = 0
+
+
+##############################
+# process_input() - processes all user input
+#
+##############################
+def process_input():
+    events = pygame.event.get()
+    for event in events:
+        #check for closing the window
+        if event.type == pygame.QUIT:
+            global running
+            running = False
+            
+
+
+##############################
+# draw() - draws objects on the screen. 
+#
+##############################
+def draw():
+    # DO NOT EDIT
+    pygame.display.flip()
+    
+    # EDIT HERE
+    screen.fill(BLACK)
+    pygame.draw.rect(screen, WHITE, (BOARDLEFT - 2, BOARDTOP - 2, BOARDWIDTH + 4, BOARDHEIGHT + 4), 1)
+    pygame.draw.rect(screen, WHITE, (BOARDRIGHT + 18, BOARDTOP - 2, 74, 94), 1)
+    render_text('NEXT', WHITE, BLACK, BOARDRIGHT + 35, BOARDTOP - 20)
+    all_sprites.draw(screen) 
+
+
+
+##############################
+# game_setup() - setting up the actual game
+#
+##############################
+def game_setup():
+    global all_sprites  # list of all sprites currently existing
+
+    all_sprites = pygame.sprite.Group()
+
+
+##############################
+# pygame_setup() - setting up the pygame environment
+#  probably shouldn't change anything here other than font
+##############################
+def pygame_setup():
+    global screen       # screen object of size WIDTH x HEIGHT
+    global font         # font type
+    global clock        # game clock
+
+    pygame.init()
+    pygame.mixer.init()
+    pygame.display.set_caption(TITLE)
+    pygame.font.init()
+
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    font = pygame.font.SysFont(None, 20)
+    clock = pygame.time.Clock()
+
+ 
+##############################
+# render_text() - render text on the screen
+# 
+##############################
+def render_text(string, fontcolor, bgcolor, x, y):
+    textsurface = font.render(string, True, fontcolor, bgcolor)
+    screen.blit(textsurface, (x,y))
+
+
+
+##############################
+# Start program
+##############################
+run()
+
+
